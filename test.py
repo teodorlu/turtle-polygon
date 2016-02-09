6a7146a0b9bac38d4a1b2b6af91469e0fc9b01fb
Teodor Heggelund <teodor.heggelund@gmail.com>
2016-02-09 20:33:34 +0100

    Pass initial two tests

---
+++ test.py
@@ -0,0 +1,33 @@
+import unittest
+import turtle_polygon
+
+			
+
+class TestTurtlePolygon(unittest.TestCase):
+
+	def assert_letter_equal(self, l1, l2):
+		for (p1, p2) in zip(l1, l2):
+			for ((x1, y1), (x2, y2)) in zip(p1, p2):
+				self.assertAlmostEqual(x1, x2)
+				self.assertAlmostEqual(y1, y2)
+
+	def test_map_letter(self):
+		def shift_x_100(tup):
+			(x, y) = tup
+			return (x+100, y)
+
+		origin = [[(0,0)]]
+		expected_shifted_origin = [[(100,0)]]
+		shifted_origin = turtle_polygon.map_letter(shift_x_100, origin)
+		self.assert_letter_equal(expected_shifted_origin, shifted_origin)
+
+	def test_translator_x(self):
+		origin = [[(0,0)]]
+		expected_shifted_origin = [[(100,0)]]
+
+		delta = (100, 0)
+		shifted_origin = turtle_polygon.translate_letter(origin, delta)
+		self.assert_letter_equal(expected_shifted_origin, shifted_origin)
+
+if __name__ == '__main__':
+	unittest.main()
---
+++ turtle_polygon.py
@@ -0,0 +1,42 @@
+from turtle import *
+
+def draw_poly(polygon):
+	if len(polygon) < 2:
+		return()
+
+	penup()
+	goto(polygon[0])
+	pendown()
+	for i in range(1, len(polygon)):
+		goto(polygon[i])
+	penup()
+
+def draw_letter(polygons):
+	for poly in polygons:
+		draw_poly(poly)
+
+def map_letter(f, letter):
+	return map(lambda poly: map(f, poly), letter)
+
+alphabet = {
+	't': [[(0, 100), (80,100)], [(40, 0), (40,100)]]	
+}
+
+def translate_letter(letter, delta):
+	def translator(tup):
+		(x, y) = tup
+		(dx, dy) = delta
+		return (x + dx, y+dy)
+
+	return map_letter(translator, letter)
+
+def main():
+
+	t2 = map_letter(lambda tup: (tup[0]+200, tup[1]), t)
+	draw_letter(t)
+	draw_letter(t2)
+
+	done()
+	
+if __name__ == '__main__':
+	main()
